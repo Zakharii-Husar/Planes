5fd14d213c278872157b7ec0b260d398571dc8d4
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-09-04 16:42:52 +0100

    fg

--- Script1.js
+++ Script1.js
@@ -425,7 +425,7 @@ document.addEventListener("keydown", keyDown);
 const fireBtn = document.getElementById("fire");
 fireBtn.addEventListener("click", playerShooting);
 const leftBtn = document.getElementById("left");
-leftBtn.addEventListener("touchstart", moveLeft);
+leftBtn.addEventListener("mousedown", moveLeft);
 const rightBtn = document.getElementById("right");
 rightBtn.addEventListener("click", moveRight)
 
