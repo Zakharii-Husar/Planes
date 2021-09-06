4a83626ef03986dbba7b7dd9964a1d97c30f23f9
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-09-07 00:13:43 +0100

    df

--- Script1.js
+++ Script1.js
@@ -454,7 +454,7 @@ document.addEventListener("keydown", keyDown);
 
         rightBtn.addEventListener("click", moveRight);
 
-    document.addEventListener("touchstart", startShooting);
+    document.addEventListener("click", startShooting);
 // BOT'S CONTROL
 
 const computerShooting = () => {
