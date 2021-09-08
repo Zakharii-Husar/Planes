97a81feb7c2ee8330557ea8a38a9e31373434be0
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-09-08 01:59:11 +0100

    df

--- Script1.js
+++ Script1.js
@@ -417,9 +417,7 @@ const crashConditions = (object) => {
                 playerBullet.h,
                 playerBullet.speed))
             setTimeout(function () { playersBulletArr.shift() }, 900);
-            shootingTimer = 0;
         };
-        shootingTimer +=1;
     };
 
 
@@ -497,7 +495,9 @@ document.addEventListener("keydown", keyDown);
                 computerBullet.h,
                 computerBullet.speed));
             setTimeout(function () { computersBulletArr.shift() }, 900);
+            shootingTimer = 0;
         }
+        shootingTimer += 1;
 };
 
 const autopilot = () => {
